22fa08fdaea5b466b9b6fd19d735aa1db50328c6
William Wang <swang45@uw.edu>
2022-01-21 16:26:01 -0800

    Completed Most Recent Crime Alert

--- src/Alert.js
+++ src/Alert.js
@@ -1,31 +1,43 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
 export function Alert(props) {
     const data = props.data;
+    const [mostRecentData, setData] = useState(null);
+
+
     if (data) {
         let sortedData = data.sort((a, b) => {
             let ADate = new Date(a.offense_start_datetime);
             let BDate = new Date(b.offense_start_datetime);
-            console.log(BDate.getTime() - ADate.getTime());
             return BDate.getTime() - ADate.getTime();
         });
-        console.log(sortedData);
+        if (!mostRecentData) {
+            setData(sortedData[0]);
+        }
     }
     return (
         <div>
             <h1>Alert The Most Recent Crime</h1>
             <div>
                 <div className="data-container">
-                    <h2>Location</h2>
-                    <p></p>
+                    <h2>Block Location</h2>
+                    <p>
+                        {mostRecentData
+                            ? mostRecentData._100_block_address
+                            : ""}
+                    </p>
                 </div>
                 <div className="data-container">
                     <h2>Time</h2>
-                    <p></p>
+                    <p>
+                        {mostRecentData
+                            ? mostRecentData.offense_start_datetime
+                            : ""}
+                    </p>
                 </div>
                 <div className="data-container">
                     <h2>Crime Type</h2>
-                    <p></p>
+                    <p>{mostRecentData ? mostRecentData.offense : ""}</p>
                 </div>
             </div>
         </div>
